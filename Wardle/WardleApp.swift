e68c613dfad7ebdd343b726a32cf699aa862ba7f
Casey Fleser <cfleser@gmail.com>
2022-08-30 05:45:26 -0500

    Initial Commit

---
+++ Wardle/WardleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardleApp.swift
+//  Wardle
+//
+//  Created by Casey Fleser on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WardleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
